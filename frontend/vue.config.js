0e7384bff595e63d486d016add71cdcd5554dc0e
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2024-10-14 22:49:35 +0700

    first commit

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    https: true, // Aktifkan HTTPS untuk dev server
+    port: 8080, // Ganti dengan port yang ingin Anda gunakan
+  },
 })
